e3fdca30c0a4188a03748785625522b45145ca78
Luchesar V. ILIEV <luchesar.iliev@gmail.com>
2018-09-12 15:57:37 +0300

    add the config itself

---
+++ ambit-mariadb/dbgrants.sql
@@ -0,0 +1,5 @@
+GRANT TRIGGER ON `ambit2`.* TO 'guest'@'%';
+GRANT EXECUTE ON PROCEDURE `ambit2`.findByProperty TO 'guest'@'%';
+GRANT EXECUTE ON PROCEDURE `ambit2`.deleteDataset TO 'guest'@'%';
+GRANT EXECUTE ON PROCEDURE `ambit2`.createBundleCopy TO 'guest'@'%';
+GRANT EXECUTE ON PROCEDURE `ambit2`.createBundleVersion TO 'guest'@'%';
